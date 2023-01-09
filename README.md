d58ba57c258ec37b480a4dee0735aec7cf638e52
luquilo <luquilo.etc@gmail.com>
2023-01-09 09:49:20 +0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # net-nitnja-webpack
+
+
+these code was just playing around with webpack
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const { resolve } = require('node:path/win32')
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    watch: true
+}
